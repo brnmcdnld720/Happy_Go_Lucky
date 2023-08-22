ccb2c810a52513047c9bfd72ad28798e9201881d
unknown <brnmcdnld720@gmail.com>
2023-08-21 22:48:49 -0600

    added happy go lucky test map

--- Overworld.js
+++ Overworld.js
@@ -6,11 +6,18 @@ class Overworld {
     }
 
     init() {
+        const backgroundScale = 1.5;
         const image = new Image();
         image.onload = () => {
-            this.ctx.drawImage(image, 0,0);
+            this.ctx.drawImage(image, 
+                0,0,
+                352 * backgroundScale, 198 * backgroundScale,
+                0, 0,
+                352, 198                
+                );
         };
-        image.src = 'Assets/01-pizza-legends-intro/images/maps/DemoLower.png'
+        image.src = 'Assets/DemoScene.png'; // 96 x 32 tiles 16px => 1536 x 512 px 22 x 20 => 352x320
+        // image.src = 'Assets/01-pizza-legends-intro/images/maps/DemoLower.png'
 
         //place some game objects!
         const hero = new GameObject({
